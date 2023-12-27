aa9487b1c818cee3db6d8429ea1dd0b2ac635252
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2023-12-27 22:59:44 +0200

    add

--- src/pages/Home.styled.js
+++ src/pages/Home.styled.js
@@ -2,31 +2,32 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const HomeTitle = styled.h1`
+  width: 700px;
+  margin: 0 auto;
   text-align: center;
   font-weight: 500;
   color: #c0bfbf;
   box-shadow: 2px 2px 10px #7be8ea;
-  border-radius: 5px;
-  font-size: 40px;
+  border-radius: 8px;
+  font-size: 35px;
   margin-top: 50px;
 `;
 
 export const HomeText = styled.p`
-  padding: 15px;
+  padding: 10px;
   width: 600px;
   margin: 0 auto;
   text-align: center;
   font-weight: 500;
   color: #c0bfbf;
   box-shadow: 2px 2px 10px #7be8ea;
-  border-radius: 20px;
-  font-size: 30px;
+  border-radius: 8px;
+  font-size: 23px;
   margin-top: 50px;
 `;
 
 export const HomeLink = styled(NavLink)`
-  color: #c0bfbf;
-  text-decoration: underline;
+  color: #87b9b3;
 
   &:hover {
     color: #7be8ea;
